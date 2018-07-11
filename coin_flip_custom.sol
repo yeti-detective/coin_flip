5845ddc4da80d552db5102cc947c7d5f801de51f
yeti-detective <yeti.detective@gmail.com>
2018-07-10 20:21:41 -0400

    custom coin flippin'

---
+++ coin_flip_custom.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.24
+
+contract CoinFlipCustom {
+
+  function placeBet(string _guess) external payable {
+    
+  }
+
+  function flipCoin() {
+
+  }
+
+  function payWinner() {}
+
+}
