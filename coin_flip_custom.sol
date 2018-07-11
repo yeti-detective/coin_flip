110b919a2baa4fc1711a3d542638608d5f629408
yeti-detective <yeti.detective@gmail.com>
2018-07-11 16:28:55 -0400

    working out these methods

--- coin_flip_custom.sol
+++ coin_flip_custom.sol
@@ -1,15 +1,30 @@
-pragma solidity ^0.4.24
+pragma solidity ^0.4.16;
 
 contract CoinFlipCustom {
+  struct game {
+    uint gameId;
+    address player;
+    string guess;
+    string result;
+  }
+  mapping (uint => game) games;
+  uint currentGameId = 0;
 
   function placeBet(string _guess) external payable {
-    
+    game storage newGame = game(
+      currentGameId,
+      msg.sender,
+      _guess,
+      flipCoin()
+      );
   }
 
-  function flipCoin() {
-
+  function flipCoin() returns(string) {
+    return "heads";
   }
 
-  function payWinner() {}
+  function payWinner() {
+
+  }
 
 }
